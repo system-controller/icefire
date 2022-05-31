77db1d28fc66e7c37beb18b9f1bb8216df947676
STD-STRING <joejoestar969@gmail.com>
2022-05-31 19:59:24 -0400

    Testing Branch Check
    Signed-off-by: STD-STRING <joejoestar969@gmail.com>

--- src/main/java/com/ikalon/icefire/IceFire.java
+++ src/main/java/com/ikalon/icefire/IceFire.java
@@ -18,7 +18,7 @@ public class IceFire implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+		LOGGER.info("blah");
 		LOGGER.info("Begin loading " + MOD_ID);
 		ModItems.registerModItems();
 		LOGGER.info("Finished loading " + MOD_ID);
