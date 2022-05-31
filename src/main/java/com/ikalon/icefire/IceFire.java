4dbe24073ee0ba79943ef4fbcca70ca5768f4631
system-controller <systemcontroller@protonmail.com>
2022-05-31 18:51:57 -0400

    ItemGroup implemented

--- src/main/java/com/ikalon/icefire/IceFire.java
+++ src/main/java/com/ikalon/icefire/IceFire.java
@@ -10,6 +10,8 @@ public class IceFire implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
 
+	public static final String MOD_ID = "icefire";
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
---
+++ src/main/java/com/ikalon/icefire/items/ModItemGroup.java
@@ -0,0 +1,14 @@
+package com.ikalon.icefire.items;
+
+import com.ikalon.icefire.IceFire;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroup {
+    public static final ItemGroup ICEFIRE_ITEMGROUP = FabricItemGroupBuilder
+            .build(new Identifier(IceFire.MOD_ID, "icefire_itemgroup"),
+                    () -> new ItemStack(Items.REDSTONE_BLOCK));
+}
